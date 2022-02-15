749126cb9a34b42e78417ae5f159335efb893001
Martin Urueta <uruetaiiimartin@gmail.com>
2022-02-15 12:30:31 -0800

    lab03

---
+++ lab03/murueta.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+using namespace std;
+
+
+int maxCross(int a[], int low, int mid, int high) { 
+    int sum = 0;
+    int Left = INT_MIN; 
+    for (int i = mid; i >= low; i--) { 
+        sum = sum + a[i]; 
+        if (sum > Left) 
+          Left = sum; 
+    } 
+  
+    sum = 0; 
+    int Right = INT_MIN;
+    for (int i = mid+1; i <= high; i++) { 
+        sum = sum + a[i]; 
+        if (sum > Right) 
+          Right = sum; 
+    } 
+  
+    return Left + Right;
+} 
+  
+
+int maxarray(int a[], int low, int high) 
+{ 
+
+   if (low == high) {
+     return a[low]; 
+    }
+
+   int mid = (low + high)/2;
+   if (maxarray(a, low, mid) >= maxarray(a, mid+1, high) && 
+       maxarray(a, low, mid) >= maxCross(a, low, mid, high))
+    {
+       return maxarray(a, low, mid);
+    }
+    
+   if (maxarray(a, low, mid) <= maxarray(a, mid+1, high) && 
+       maxarray(a, mid+1, high) >= maxCross(a, low, mid, high))
+    {
+       return maxarray(a, mid+1, high);
+    }
+    
+   if (maxCross(a, low, mid, high) >= maxarray(a, mid+1, high) && 
+       maxarray(a, low, mid) <= maxCross(a, low, mid, high))
+    {
+       return maxCross(a, low, mid, high);
+    }
+return 0;
+}
+
+int main() 
+{ 
+    int Arraysize;
+    cin >> Arraysize;
+        
+    int a[Arraysize];
+        
+        for (int i = 0; i < Arraysize; i++) {
+            cin >> a[i];
+        }
+           int Maximized_sum = maxarray(a, 0, Arraysize-1);
+           cout<< Maximized_sum; 
+           return Maximized_sum; 
+}
